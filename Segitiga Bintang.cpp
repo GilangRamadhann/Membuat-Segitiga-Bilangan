717aaacd2d9979ba31444a01771e7c2866e4216a
Gilang Ramadhan <92220215+GilangRamadhann@users.noreply.github.com>
2021-12-06 12:27:15 +0700

    Add files via upload

---
+++ Segitiga Bintang.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,x;
+    cout<<"Inputkan Angka = ";
+    cin>>x;
+    cout<<endl;
+    for(a=1;a<=x;a++){
+        for(b=a;b<=x;b++){
+            cout<<b;
+            if(b!=x)cout<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
